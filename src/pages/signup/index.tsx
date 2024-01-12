24de770f53d387deff8c328552b801ddf34d8ad8
larmong <larmong@naver.com>
2024-01-12 23:26:03 +0900

    feat: signup page

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,3 +1,66 @@
+import { Button, Form, Input } from 'antd';
+
+import { Title, Wrapper } from './style';
+import SignupButton from 'components/button';
+
 export default function Signup(): JSX.Element {
-  return <>Signup</>;
+  const [signupForm] = Form.useForm();
+
+  const handleClickSignup = async (values: any) => {
+    console.log(values);
+  };
+
+  return (
+    <Wrapper>
+      <Form form={signupForm} onFinish={handleClickSignup}>
+        <Form.Item
+          name="email"
+          rules={[
+            { required: true, message: '이메일을 입력해주세요.' },
+            {
+              type: 'email',
+              message: '아이디를 이메일 형식으로 입력해주세요.',
+            },
+          ]}
+        >
+          <div>
+            <Title>email</Title>
+            <div className="id-line">
+              <Input className="form-input" placeholder="이메일을 입력해주세요." />
+              <SignupButton text="중복확인" class="line" />
+            </div>
+          </div>
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            { required: true, message: '비밀번호를 입력해주세요.' },
+            { min: 6, message: '비밀번호를 6글자 이상 입력해주세요.' },
+          ]}
+        >
+          <div>
+            <Title>password</Title>
+            <Input.Password className="form-input" placeholder="비밀번호를 입력해주세요." />
+          </div>
+        </Form.Item>
+        <Form.Item name="name" rules={[{ required: true, message: '이름을 입력해주세요.' }]}>
+          <div>
+            <Title>name</Title>
+            <Input className="form-input" placeholder="이름을 입력해주세요." />
+          </div>
+        </Form.Item>
+        <Form.Item name="phone" rules={[{ required: true, message: '연락처를 입력해주세요.' }]}>
+          <div>
+            <Title>phone</Title>
+            <Input className="form-input" placeholder="연락처를 입력해주세요." />
+          </div>
+        </Form.Item>
+        <Form.Item>
+          <Button htmlType="submit" className="ant-btn-reset">
+            <SignupButton text="Signup" />
+          </Button>
+        </Form.Item>
+      </Form>
+    </Wrapper>
+  );
 }
---
+++ src/pages/signup/style.ts
@@ -0,0 +1,29 @@
+import styled from '@emotion/styled';
+import { Common } from '../../commons/style/emotion';
+
+export const Wrapper = styled.div`
+  margin: 40px auto;
+  width: 400px;
+  .ant-btn-reset {
+    height: 40px;
+    font-size: 16px;
+    margin-top: 20px;
+  }
+  .id-line {
+    display: flex;
+    gap: 16px;
+    .line {
+      width: 120px;
+      height: 40px;
+      font-size: 14px;
+    }
+  }
+`;
+
+export const Title = styled.div`
+  display: block;
+  text-transform: uppercase;
+  color: ${Common.color.darkPoint};
+  font-weight: 700;
+  margin: 5px 0;
+`;
